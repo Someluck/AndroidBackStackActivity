322665681e79952ea2422c94105daea82d8dd428
Someluck <ivanzablotskiy@gmail.com>
2016-10-03 23:11:44 +0300

    no message

---
+++ AndroidBackStackActivity/app/src/main/java/by/ivanzablotskiy/androidbackstackactivity/FifthActivity.java
@@ -0,0 +1,17 @@
+package by.ivanzablotskiy.androidbackstackactivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ivanz on 03.10.2016.
+ */
+
+public class FifthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ AndroidBackStackActivity/app/src/main/java/by/ivanzablotskiy/androidbackstackactivity/SecondActivity.java
@@ -0,0 +1,16 @@
+package by.ivanzablotskiy.androidbackstackactivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ivanz on 03.10.2016.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
