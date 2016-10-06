20e93f0fad158c36ae82aee00aa2a3d2cee3454d
Someluck <ivanzablotskiy@gmail.com>
2016-10-06 23:55:27 +0300

    2 twitter layouts add

--- AndroidBackStackActivity/app/src/main/java/by/ivanzablotskiy/androidbackstackactivity/TwitterActivity.java
+++ AndroidBackStackActivity/app/src/main/java/by/ivanzablotskiy/androidbackstackactivity/TwitterActivity.java
@@ -1,20 +1,41 @@
 package by.ivanzablotskiy.androidbackstackactivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by ivanz on 06.10.2016.
- */
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class TwitterActivity extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.twitter_layout);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        TabHost tabHost = (TabHost)findViewById(R.id.tabHost);
+
+        tabHost.setup();
+
+        TabSpec spec1 = tabHost.newTabSpec("Tab1");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator("", getResources().getDrawable(R.drawable.ic_bar_home));
+
+        TabSpec spec2 = tabHost.newTabSpec("Tab2");
+        spec2.setContent(R.id.tab1);
+        spec2.setIndicator("", getResources().getDrawable(R.drawable.ic_bar_messages));
+
+        TabSpec spec3 = tabHost.newTabSpec("Tab3");
+        spec3.setContent(R.id.tab1);
+        spec3.setIndicator("", getResources().getDrawable(R.drawable.ic_bar_notifications));
+
+        tabHost.addTab(spec1);
+        tabHost.addTab(spec2);
+        tabHost.addTab(spec3);
     }
 }
